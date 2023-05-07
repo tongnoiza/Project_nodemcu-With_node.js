c9b8885bcab36d37bd46c7f1132ed897c16ad3c3
sontaya <sontaya@aspiron.co.th>
2023-05-07 17:43:05 +0700

    add db

--- motorLog/MotorDB.js
+++ motorLog/MotorDB.js
@@ -1,8 +1,8 @@
 import { Sequelize, Model, DataTypes } from 'sequelize';
 
-        const sequelize = new Sequelize('testdb', 'root', '', {
-            host: 'localhost',
-            dialect:  'mysql'
+        const sequelize = new Sequelize('testdb_cul2', 'sontaya', 'edOrR8vOo1Hnq6qi068rp8Gl398zbNLe', {
+            host: 'dpg-chbnv8e7avjcvo70hjpg-a',
+            dialect:  'postgres'
           });
           let Motorlogging
         try {
--- user/UserDB.js
+++ user/UserDB.js
@@ -1,9 +1,9 @@
 "use strict"
 import { Sequelize, Model, DataTypes } from 'sequelize';
 
-        const sequelize = new Sequelize('testdb', 'root', '', {
-            host: 'localhost',
-            dialect:  'mysql'
+        const sequelize = new Sequelize('testdb_cul2', 'sontaya', 'edOrR8vOo1Hnq6qi068rp8Gl398zbNLe', {
+          host: 'dpg-chbnv8e7avjcvo70hjpg-a',
+            dialect:  'postgres'
           });
           let Userdb
         try {
