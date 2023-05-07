0808f988face460de7f48c2017a727a2f85b4ecd
sontaya <sontaya@aspiron.co.th>
2023-05-07 18:48:59 +0700

    push

--- post/postdb.js
+++ post/postdb.js
@@ -2,6 +2,7 @@ import { Sequelize, Model, DataTypes } from 'sequelize';
 
         const sequelize = new Sequelize('testdb_cul2', 'sontaya', 'edOrR8vOo1Hnq6qi068rp8Gl398zbNLe', {
             host: 'dpg-chbnv8e7avjcvo70hjpg-a',
+            port:5432,
             dialect:  'postgres'
           });
               let post
