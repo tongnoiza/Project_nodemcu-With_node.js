7c8ad9cf545073baa863ccf03d598969d44111c4
sontaya <sontaya@aspiron.co.th>
2023-05-10 19:19:17 +0700

    change db

--- configdb.js
+++ configdb.js
@@ -1,13 +1,13 @@
 import { Sequelize, Model, DataTypes } from 'sequelize';
 
-        const sequelize = new Sequelize('testdb_cul2', 'sontaya', 'edOrR8vOo1Hnq6qi068rp8Gl398zbNLe', {
-            host: 'dpg-chbnv8e7avjcvo70hjpg-a',
-            port:5432,
-            dialect:  'postgres'
-          });
-        // const sequelize = new Sequelize('testdb', 'root', '', {
-        //   host: 'localhost',
-        //   dialect:  'mysql'
-        // });
+        // const sequelize = new Sequelize('testdb_cul2', 'sontaya', 'edOrR8vOo1Hnq6qi068rp8Gl398zbNLe', {
+        //     host: 'dpg-chbnv8e7avjcvo70hjpg-a',
+        //     port:5432,
+        //     dialect:  'postgres'
+        //   });
+        const sequelize = new Sequelize('testdb', 'root', '', {
+          host: 'localhost',
+          dialect:  'mysql'
+        });
         
         export default sequelize
