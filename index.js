47869e0c3ca1204e4d8d71101cea72c313ae62bc
sontaya <sontaya@aspiron.co.th>
2023-05-11 14:08:20 +0700

    change port

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express  from 'express'
-import Post from './post/postcontroller.js';
-import User from './user/UserController.js';
+// import Post from './post/postcontroller.js';
+// import User from './user/UserController.js';
+// import comment from './comment/comment.js';
+
 import Motor from './motorLog/Motorcontroller.js';
-import comment from './comment/comment.js';
 import bodyParser from 'body-parser'
 import cors from 'cors'
 const app = express()
-app.use(cors({
-  origin:'*'
-}))
+app.use(cors())
 // app.use(function(req, res, next) {
 //   res.setHeader('Access-Control-Allow-Origin', '*'); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
 //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -18,12 +17,12 @@ app.use(cors({
 // });
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json());
-const port = process.env.PORT 
-app.use('/post',Post)
-app.use('/User',User)
-app.use('/LogState',Motor)
-app.use('/Comment',comment)
+const port = process.env.PORT ||443
+// app.use('/post',Post)
+// app.use('/User',User)
+// app.use('/Comment',comment)
 
+app.use('/LogState',Motor)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
