266d4aca21ed579ecf200ea1da0c1f68e9bb03f1
sontaya <sontaya@aspiron.co.th>
2023-05-11 13:04:15 +0700

    reportr code

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ import User from './user/UserController.js';
 import Motor from './motorLog/Motorcontroller.js';
 import comment from './comment/comment.js';
 import bodyParser from 'body-parser'
+import cors from 'cors'
 const app = express()
-
+app.use(cors())
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json());
 const port = 3000 | process.env.port
