a1479edeb1eb8550c9dd779a5a38239e406a320a
sontaya <sontaya@aspiron.co.th>
2023-05-11 15:34:02 +0700

    config db

--- configdb.js
+++ configdb.js
@@ -6,6 +6,11 @@ import { Sequelize, Model, DataTypes } from 'sequelize';
             dialect:  'postgres'
           });
           
+          // const sequelize = new Sequelize('verceldb', 'default', '4GRWrjXS2chT', {
+          //   host: 'ep-lingering-pine-802136-pooler.ap-southeast-1.postgres.vercel-storage.com',
+          //   port:5432,
+          //   dialect:  'postgres'
+          // });
         // const sequelize = new Sequelize('testdb', 'root', '', {
         //   host: 'localhost',
         //   dialect:  'mysql'
